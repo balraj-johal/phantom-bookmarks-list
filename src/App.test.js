548ebe8343d2b66e9ff21167cb59ed1a2c9c31a4
Balraj Johal <balrajsjohal@gmail.com>
2022-08-04 19:19:52 +0100

    initialise repo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders application div', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const applicationRootElement = screen.getByTestId("App");
+  expect(applicationRootElement).toBeInTheDocument();
 });
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+function Header() {
+    return(
+        <header>
+            <h1>
+                Bookmarks List
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
