e77e4300cb1c4801f0f5e3cb50ff27787ba26c16
Balraj Johal <balrajsjohal@gmail.com>
2022-08-05 20:31:24 +0100

    add error feedback to add link form

--- src/components/AddLinkForm.js
+++ src/components/AddLinkForm.js
@@ -4,13 +4,14 @@ import { useNavigate } from "react-router-dom";
 function AddLinkForm(props) {
   const navigate = useNavigate();
   const [url, setURL] = useState("");
+  const [error, setError] = useState("");
 
   const onSubmit = async (e) => {
     e.preventDefault();
     // attempt to save link
     try {
       const error = await props.addLink({url});
-      if (error) return console.log(error);
+      if (error) return setError(error);
       // redirect to success page if link saved
       navigate(`/link-saved/${encodeURIComponent(url)}`);
     } catch (error) {
@@ -20,11 +21,17 @@ function AddLinkForm(props) {
 
   return(
     <form className="bookmark-link add-link" onSubmit={onSubmit}>
-      <input 
-        type="text" 
-        value={url}
-        onChange={e => setURL(e.target.value)} 
-      />
+      <div>
+        <input 
+          type="text" 
+          value={url}
+          onChange={e => setURL(e.target.value)} 
+          required
+        />
+        <span aria-live="assertive">
+          {error}
+        </span>
+      </div>
       <button type="submit">
         Save Link
       </button>
