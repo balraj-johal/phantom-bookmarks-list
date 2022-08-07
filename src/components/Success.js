0f92945c242572fb6a41e51be18c055ed6579aea
Balraj Johal <balrajsjohal@gmail.com>
2022-08-07 18:18:31 +0100

    add success animation

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,5 +1,9 @@
 import { useParams } from "react-router-dom";
+
+import successAnim from "../resources/animations/success.json";
+
 import BookmarkLink from "./BookmarkLink";
+import Lottie from "react-lottie-player";
 
 function Success() {
   const routeParams = useParams();
@@ -7,7 +11,15 @@ function Success() {
   return(
     <main className="success">
       <h1>Your link has been successfully added!</h1>
-      <div className="success-anim"></div>
+      <Lottie 
+          rendererSettings={{ 
+              preserveAspectRatio: 'xMidYMid slice' 
+          }}
+          className="success-anim"
+          animationData={successAnim} 
+          play
+          loop={false}
+      />
       <BookmarkLink
         link={{ url: decodeURIComponent(routeParams.linkID) }}
       />
